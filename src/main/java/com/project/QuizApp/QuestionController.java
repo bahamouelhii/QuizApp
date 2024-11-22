318c24aee2e79e519eb0a851289c3649d6237486
baha_mouelhi <bahaeddine.mouelhi@soprahr.com>
2024-11-22 20:31:35 +0100

    get by id

--- src/main/java/com/project/QuizApp/QuestionController.java
+++ src/main/java/com/project/QuizApp/QuestionController.java
@@ -2,6 +2,8 @@ package com.project.QuizApp;
 
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,5 +19,9 @@ public class QuestionController {
         return questionService.getAllQuestions();
     }
 
+    @GetMapping("/{id}")
+    public Question getQuestionById(@PathVariable int id) {
+        return questionService.getQuestionById(id);
+    }
 
 }
